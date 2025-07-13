546254b9713802717f10bf86918e397f73815d84
edwindiazmarin <70909022+edwin-gif@users.noreply.github.com>
2025-07-12 22:17:33 -0600

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/veterinaria_qr/public/';
+    public string $baseURL = 'https://mitotli.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
